3d4861b18ca540ab08e1943908e0e04768db7e7b
Vlad Avseev <vladavseev47@gmail.com>
2026-08-03 20:46:55 +0300

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import prettierConfig from 'eslint-config-prettier';
+
+export default tseslint.config(
+  { ignores: ['dist', 'node_modules'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: { react: react },
+    settings: { react: { version: 'detect' } },
+    languageOptions: {
+      parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+    rules: {
+      ...react.configs.flat.recommended.rules,
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  {
+    plugins: { 'react-hooks': reactHooks },
+    rules: { ...reactHooks.configs.recommended.rules },
+  },
+  prettierConfig,
+);
